8baa9d8864559ada58b47bea4baa94c1790fb878
rtem114 <rtem114@gmail.com>
2021-11-12 01:53:14 +0200

    Added tests (#1)
    Co-authored-by: arkharlamov <arkharlamov@homeaway.com>

--- src/main/java/org/manifest/manifestcomparisontool/camel/processor/ComparisionProcessor.java
+++ src/main/java/org/manifest/manifestcomparisontool/camel/processor/ComparisionProcessor.java
@@ -17,16 +17,16 @@ public class ComparisionProcessor implements Processor {
     @Override
     public void process(Exchange exchange) {
         List<Exchange> bodyList = exchange.getIn().getBody(List.class);
-        Manifest man1 = bodyList.get(0).getIn().getBody(Manifest.class);
-        Manifest man2 = bodyList.get(1).getIn().getBody(Manifest.class);
+        Manifest manifest1 = bodyList.get(0).getIn().getBody(Manifest.class);
+        Manifest manifest2 = bodyList.get(1).getIn().getBody(Manifest.class);
 
         //MANIFEST_HEADER identifies what file we compare with. FILE
         //FILE_1 means that we compare  FILE_1 with FILE_2.
         //FILE_2 means that we compare  FILE_2 with FILE_1.
         Object manifestFileOutput = bodyList.get(0).getIn().getHeader(ManifestUtil.MANIFEST_HEADER);
 
-        //calculate the difference
-        Manifest diffManifest = ManifestUtil.differentiate(man1, man2);
+        //calculates the difference
+        Manifest diffManifest = ManifestUtil.differentiate(manifest1, manifest2);
 
         exchange.getIn().setBody(ManifestUtil.toString(diffManifest));
         //set manifest file output header, so the differences will be stored in corresponding file
